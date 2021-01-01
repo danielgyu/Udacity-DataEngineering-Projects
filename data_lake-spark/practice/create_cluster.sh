ba28786c797e2d9056b4674f64ac668fb8b60264
kungyu <hismayfly@gmail.com>
2021-01-01 10:36:18 +0900

    [MODIFY : KG] finished final project

--- data_lake-spark/practice/create_cluster.sh
+++ data_lake-spark/practice/create_cluster.sh
@@ -3,7 +3,7 @@ aws emr create-cluster --name spark-emr \
 --release-label emr-5.21.0 \
 --instance-count 4 \
 --applications Name=Spark \
---bootstrap-actions Path=s3://udacity-emr/spark_bootstrap.sh \
+--bootstrap-actions Path=s3://udacity-emr/sparkify/spark_bootstrap.sh \
 --ec2-attributes KeyName=udacity-spark \
 --instance-type m5.xlarge \
 
